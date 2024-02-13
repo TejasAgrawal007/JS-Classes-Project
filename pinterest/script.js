c06c0d6b7bf4d1e94306184c9d68383742ff4f25
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2024-02-13 11:21:07 +0530

    Update script.js

--- pinterest/script.js
+++ pinterest/script.js
@@ -35,3 +35,22 @@ var arr = [
       "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
+
+
+
+function showCards(){
+  let clutter = ""
+  arr.forEach(function(e){
+    clutter += `<div class="box">
+                  <img src="${e.image}" alt="">
+                  <div class="caption">Lorem, ipsum.</div>
+                </div>`
+  })
+
+  document.querySelector(".container")
+              .innerHTML = clutter;
+
+}
+
+
+showCards();
