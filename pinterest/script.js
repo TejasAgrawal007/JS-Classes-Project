ba5f89a9f538ff983db09d18cc9a5fb7f633d654
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2025-02-19 10:37:38 -0800

    Update script.js

--- pinterest/script.js
+++ pinterest/script.js
@@ -1,56 +1,87 @@
 var arr = [
-  {
-    name: "Petals of roses",
-    image:
-      "https://images.unsplash.com/photo-1518621736915-f3b1c41bfd00?q=80&w=3786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Animals of town",
-    image:
-      "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=2688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "the crowd of city",
-    image:
-      "https://images.unsplash.com/photo-1517732306149-e8f829eb588a?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "fruits of planet",
-    image:
-      "https://images.unsplash.com/photo-1618897996318-5a901fa6ca71?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "orange peeled",
-    image:
-      "https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=3337&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "web design",
-    image:
-      "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "apple juice",
-    image:
-      "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
+    {
+        name: "Petals of roses",
+        image:
+            "https://images.unsplash.com/photo-1518621736915-f3b1c41bfd00?q=80&w=3786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "Animals of town",
+        image:
+            "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=2688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "the crowd of city",
+        image:
+            "https://images.unsplash.com/photo-1517732306149-e8f829eb588a?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "fruits of planet",
+        image:
+            "https://images.unsplash.com/photo-1618897996318-5a901fa6ca71?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "orange peeled",
+        image:
+            "https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=3337&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "web design",
+        image:
+            "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "apple juice",
+        image:
+            "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
 ];
 
 
 
-function showCards(){
-  let clutter = ""
-  arr.forEach(function(e){
-    clutter += `<div class="box">
-                  <img src="${e.image}" alt="">
-                  <div class="caption">Lorem, ipsum.</div>
-                </div>`
-  })
+function showCards() {
+    let clutter = ""
+    arr.forEach(function (e) {
+        clutter += `<div class="box">
+                    <img src="${e.image}" alt="">
+                    <div class="caption">Lorem, ipsum.</div>
+                  </div>`
+    })
 
-  document.querySelector(".container")
-              .innerHTML = clutter;
+    document.querySelector(".container")
+        .innerHTML = clutter;
 
 }
 
 
+function handleSearchFunctionality() {
+
+    let input = document.querySelector("#searchinput");
+
+    input.addEventListener("focus", () => {
+        document.querySelector(".overlay").style.display = "block";
+    })
+    input.addEventListener("blur", () => {
+        document.querySelector(".overlay").style.display = "none";
+    });
+
+    input.addEventListener("input", function () {
+
+        const result = arr.filter(items => items.name.toLowerCase().startsWith(input.value));
+
+        let clutter = ""
+
+        result.forEach((items) => {
+            clutter += ` <div class="res flex px-8 py-3">
+                            <i class="ri-search-line font-semibold mr-5"></i>
+                            <h3 class="font-semibold">${items.name}</h3>
+                        </div>`
+        })
+
+        document.querySelector(".searchdata").style.display = "block"
+        document.querySelector(".searchdata").innerHTML = clutter
+
+    })
+}
+
+handleSearchFunctionality()
 showCards();
